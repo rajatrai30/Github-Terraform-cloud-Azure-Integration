eb521302a5086614256285d8ad3d29922db11838
Rajat Rai <75417243+rajatrai30@users.noreply.github.com>
2026-07-20 17:37:03 +0530

    Test-1

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
   tags = {
-    "Environment" = "Test"
+    "Environment" = "Test-1"
     "Purpose"     = "GitHub-Terraform-Azure-Integration-demo"
   }
 }
